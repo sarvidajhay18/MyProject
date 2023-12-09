6d3b9504e24af4341438626b29c5cf4f7b294243
sarvidajhay18 <152369244+sarvidajhay18@users.noreply.github.com>
2023-12-09 14:43:59 +0800

    Update app.py

--- app.py
+++ app.py
@@ -17,32 +17,13 @@ def load_lottieurl(url):
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-    from PIL import Image
-import requests
-import streamlit as st
-from streamlit_lottie import st_lottie
-from st_on_hover_tabs import on_hover_tabs
-
-st.set_page_config(page_title="My Webpage", page_icon=":tada:", layout="wide")
-
-def load_lottieurl(url):
-    r = requests.get(url)
-    if r.status_code != 200:
-        return None
-    return r.json()
-
-# use local Css
-
-def local_css(file_name):
-    with open(file_name) as f:
-        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
     
 local_css("style/style.css")
 
 # --- Load Assets ---
 lottie_coding = load_lottieurl("https://lottie.host/a796d5ff-a73b-41c8-acd6-0628114760e7/jRTduUF2dv.json")
-img_project1 = Image.open("images/scouts.png")
-img_lottie_animation = Image.open("images/Rank.png")
+img_project1 = Image.open("images/snake.jpg")
+img_lottie_animation = Image.open("images/programmer2.jpg")
 
 # --- Side Bar Section ---
 with st.sidebar:
@@ -75,7 +56,7 @@ if tabs =='Home':
         column_left, column_right = st.columns((2,1))
         with column_left:
             st.write("---")
-            st.title("Welcome to My Home Page")
+            st.title("Welcome To My Home Page")
             st.subheader("hi :wave:, I am Jhay-r C. Sarvida")
             st.write("I am a Computer Engineering students from SNSU")
             st.write("Visit my github link below to learn about my work.")
@@ -94,22 +75,31 @@ elif tabs == 'Project':
             # insert image
             st.image(img_project1)
         with text_column:
-            st.subheader("Boy Scouts of the Philippines")
-            st.write("Age range")
-            st.write("KID Scout: 4-6.")
-            st.write("KAB Scout: 6-9.")
-            st.write("Boy Scout: 9-12.")
-            st.write("Senior Scout: 13-19.")
-            st.write("Rover Scout: 18-26.")
-            
-            st.markdown("[Scouts.org.ph.](http://scouts.org.ph/)")
+            st.subheader("Snake Game App")
+            st.write(
+                """ 
+                This is a Snake Game App made in Python. 
+                """
+                """ 
+                This web app is not fully finish. 
+                """
+                """
+                This is a sample for fun web app only. 
+                """
+            )
+            st.markdown("[Github Link](https://github.com/sarvidajhay18/MyProject)")
     with st.container():
         image_column, text_column = st.columns((1,2)) # (image_column size, text_column size)
         with image_column:
             st.image(img_lottie_animation) # insert image
         with text_column:
-            st.subheader("Advancement in Boy/Senior Scouting")
-        
+            st.subheader("How to add a contact form in your Streamit App")
+            st.write(
+                """
+                Want to add a contact form to your streamlit website.
+                In this video, I'm going to show you how to implement a contact form in your Streamlit app using the free service 'Form Submit'.
+                """
+            )
             st.markdown("[Watch Here](https://youtu.be/FOULV9Xij_8)")
 
 # --- About Section ---
@@ -146,3 +136,4 @@ elif tabs == 'Feedback':
         st.markdown(contact_form, unsafe_allow_html=True)
     with right_column:
         st.empty()
+
